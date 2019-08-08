8f8facb45fbe93461fee2489c4d0935f7e95292c
marcocrepaldi <marcocrepaldi@me.com>
2019-08-07 21:26:47 -0300

    chore:Configuracao MongoDB

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,10 +6,14 @@ import File from '../models/File';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
-      where: { user_id: req.userId, canceled_at: null },
+      where: { user_id: req.user_id, canceled_at: null },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
 
       include: [
         {
@@ -54,7 +58,6 @@ class AppointmentController {
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
     }
-
     /**
      * Check for past dates
      */
@@ -62,7 +65,7 @@ class AppointmentController {
     const hourStart = startOfHour(parseISO(date));
 
     if (isBefore(hourStart, new Date())) {
-      return res.status(400).json({ error: ' Pasrt dates area note permited' });
+      return res.status(400).json({ error: ' Parse dates area note permited' });
     }
 
     /**
--- src/routes.js
+++ src/routes.js
@@ -6,20 +6,26 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
-import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
-routes.get('/providers', ProviderController.index);
-routes.post('/appointment', AppointmentController.store);
-routes.get('/appointment', AppointmentController.index);
 routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointment', AppointmentController.store);
+
+routes.get('/appointment', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
